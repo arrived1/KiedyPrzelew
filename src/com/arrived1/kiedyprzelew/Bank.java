608ab3bb2fdd2ff2a78e66fc633af09a083045f1
arrived1 <arrived1@gmail.com>
2013-01-25 23:24:47 +0100

    new idea of Bank class

--- src/com/arrived1/kiedyprzelew/Bank.java
+++ src/com/arrived1/kiedyprzelew/Bank.java
@@ -1,72 +1,53 @@
 package com.arrived1.kiedyprzelew;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Vector;
+
 public class Bank {
 	private String BankName = "";
-	private MyTime timeOut1;
-	private MyTime timeOut2;
-	private MyTime timeOut3;
-	private MyTime timeIn1;
-	private MyTime timeIn2;
-	private MyTime timeIn3;
+	private Vector<Date> TimeOut = new Vector<Date>();
+	private Vector<Date> TimeIn = new Vector<Date>();
 	
-	//TODO: What with that shit?
-	@SuppressWarnings("unused")
-	public Bank(String BankName_, 
-				MyTime timeOut1_, MyTime timeOut2_, MyTime timeOut3_, 
-				MyTime timeIn1_, MyTime timeIn2_, MyTime timeIn3_) {
-		String BankName = BankName_;
-		MyTime timeOut1 = timeOut1_;
-		MyTime timeOut2 = timeOut2_;
-		MyTime timeOut3 = timeOut3_;
-		MyTime timeIn1 = timeIn1_;
-		MyTime timeIn2 = timeIn2_;
-		MyTime timeIn3 = timeIn3_;
+	public Bank(String bankName) {
+		String[] tmp = bankName.split(",");
+		BankName = tmp[0];
+		TimeOut = setVector(tmp[1]);
+		TimeIn = setVector(tmp[2]);
 	}
 	
-	private int calcullateDifference(MyTime t1, MyTime t2) {
-		int min = Math.abs(t1.getMin() - t2.getMin());
-		int hour = Math.abs(t1.getHour() - t2.getHour());
-		
-		return hour * 60 + min;
+	//TODO: implement me
+	public Date getNearestIncomingTime(Date t) {
+		return new Date();
 	}
 	
-	private int min(int i1, int i2, int i3) {
-		return Math.min(Math.min(i1, i2), i3);
-	}
-	
-	public MyTime nearestOutgoingTime(MyTime transferTime) {
-		int diff1 = calcullateDifference(transferTime, timeOut1);
-		int diff2 = calcullateDifference(transferTime, timeOut2);
-		int diff3 = calcullateDifference(transferTime, timeOut3);
-		
-		int minValue = min(diff1, diff2, diff3);
-		
-		if(minValue == diff1)
-			return timeOut1;
-		else if(minValue == diff2)
-			return timeOut2;
-		else if(minValue == diff3)
-			return timeOut3;
-		return new MyTime(0, 0);
-	}
-	
-	public MyTime nearestIncomigTime(MyTime transferTime) {
-		int diff1 = calcullateDifference(transferTime, timeIn1);
-		int diff2 = calcullateDifference(transferTime, timeIn2);
-		int diff3 = calcullateDifference(transferTime, timeIn3);
-		
-		int minValue = min(diff1, diff2, diff3);
-		
-		if(minValue == diff1)
-			return timeIn1;
-		else if(minValue == diff2)
-			return timeIn2;
-		else if(minValue == diff3)
-			return timeIn3;
-		return new MyTime(0, 0);
+	//TODO: implement me	
+	public Date getNearestOutgoingTime(Date t) {
+		return new Date();
 	}
 	
 	public String getName() {
 		return BankName;
+	}
+	
+	private Vector<Date> setVector(String t) {
+		Vector<Date> vec = new Vector<Date>();
+		String[] times = t.split(" ");
+		SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+		
+		try {
+			//TODO: it wont works with 2, 3, 4?
+			for(int i = 0; i < 4; i++) {
+				Date time = timeFormat.parse(times[i]);
+				vec.addElement(time);
+			}
+		} 
+		catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return vec;
 	}
 }
---
+++ src/com/arrived1/kiedyprzelew/BankDatabase.java
@@ -0,0 +1,43 @@
+package com.arrived1.kiedyprzelew;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Vector;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+
+public class BankDatabase {
+	private Vector<Bank> banks = new Vector<Bank>();
+	private Activity actv;
+	
+	public BankDatabase(Activity activity) {
+		this.actv = activity;
+		try {
+			ReadFromFile();
+		} 
+		catch (IOException e) {
+			Toast.makeText(actv.getApplicationContext(), "Problems: " + e.getMessage(), Toast.LENGTH_LONG).show();
+		}
+	}
+	
+	private void ReadFromFile() throws IOException {
+		String str = "";
+		
+		InputStream is = actv.getResources().openRawResource(R.drawable.banks);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		
+		if (is != null) {							
+			while ((str = reader.readLine()) != null) {	
+				Bank bank = new Bank(str);
+				banks.addElement(bank);
+				System.out.println("DUPA, " + " Bank name: " + bank.getName());
+			}				
+		}		
+		is.close();		
+	}
+}
