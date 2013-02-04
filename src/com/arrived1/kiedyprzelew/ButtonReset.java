0ca143e6434dc68080b70eb3f9af5a746e639341
arrived1 <arrived1@gmail.com>
2013-02-04 21:25:29 +0100

    added reset functionality

--- src/com/arrived1/kiedyprzelew/ButtonReset.java
+++ src/com/arrived1/kiedyprzelew/ButtonReset.java
@@ -2,7 +2,8 @@ package com.arrived1.kiedyprzelew;
 
 import android.app.Activity;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ButtonReset extends MyButton {
 
@@ -11,6 +12,15 @@ public class ButtonReset extends MyButton {
 	}
 	
 	public void onClick(View arg0) { 
-		Toast.makeText(actv.getApplicationContext(), "Dziala, button Reset!", Toast.LENGTH_SHORT).show(); 
+		TextView nastepnegoDnia = (TextView)actv.findViewById(R.id.nextDay);
+		nastepnegoDnia.setText("");
+		
+		TextView pokazCzas = (TextView)actv.findViewById(R.id.deliveryTime);
+		pokazCzas.setText("");
+		
+		Spinner spinn1 = (Spinner)actv.findViewById(R.id.spinnerBank1);
+		spinn1.setSelection(0,true);
+		Spinner spinn2 = (Spinner)actv.findViewById(R.id.spinnerBank2);
+		spinn2.setSelection(0,true);
 	}
 }
